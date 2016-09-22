b2d6f06a06a31639732af62711d46222c9c9794d
Andrew <agusev@thumbtack.net>
2016-09-22 14:30:33 +0300

    Fail first

--- src/com/tt/training/sgu/Calc.java
+++ src/com/tt/training/sgu/Calc.java
@@ -1,10 +1,21 @@
 package com.tt.training.sgu;
 
+import java.io.InputStream;
+
 public class Calc {
+
+	public Calc(InputStream input) {
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public Double result() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
