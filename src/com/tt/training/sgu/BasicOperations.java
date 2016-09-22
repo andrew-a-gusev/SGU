0bacb40cb9439a053fab9c8e4736752708cb519a
Andrew <agusev@thumbtack.net>
2016-09-22 14:31:05 +0300

    Fail first

--- src/com/tt/training/sgu/BasicOperations.java
+++ src/com/tt/training/sgu/BasicOperations.java
@@ -2,13 +2,48 @@ package com.tt.training.sgu;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Before;
 import org.junit.Test;
 
+
 public class BasicOperations {
+	
+	private class InputStreamMock extends InputStream {
+		
+		String buffer;
+		
+		public void write(String str) {
+			buffer = str;
+		}
+
+		@Override
+		public int read() throws IOException {
+			// TODO Auto-generated method stub
+			int ret = 0;
+			if (buffer.length()>0)
+					ret = buffer.getBytes()[0];
+			buffer = buffer.substring(1);
+			return ret;
+		}
+		
+	}
+	
+	private Calc calc;
+	private InputStreamMock input;
+
+	@Before
+	public void initCalc() {
+		input = new InputStreamMock();
+		calc = new Calc(input);
+	}
 
 	@Test
 	public void add() {
-		fail("Not yet implemented");
+		input.write("1+1");
+		assertEquals(new Double(2),calc.result());
 	}
 	
 	@Test
@@ -18,12 +53,12 @@ public class BasicOperations {
 	
 	@Test
 	public void divide() {
-		
+		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void multiply() {
-		
+		fail("Not yet implemented");
 	}
 
 }
