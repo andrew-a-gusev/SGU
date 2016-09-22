e80b76e448aa0e70d028426ae512f5167deeea90
Andrew <agusev@thumbtack.net>
2016-09-22 14:07:28 +0300

    Init project

---
+++ src/com/tt/training/sgu/BasicOperations.java
@@ -0,0 +1,29 @@
+package com.tt.training.sgu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BasicOperations {
+
+	@Test
+	public void add() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void substract() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void divide() {
+		
+	}
+	
+	@Test
+	public void multiply() {
+		
+	}
+
+}
---
+++ src/com/tt/training/sgu/Calc.java
@@ -0,0 +1,10 @@
+package com.tt.training.sgu;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
